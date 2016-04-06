60ab595ba76f315a6293424a5e8f7b0d88949248
Sam Verschueren <sam.verschueren@gmail.com>
2016-04-06 08:46:22 +0200

    :arrow_up: aws-swagger-import@0.3.0

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const meow = require('meow');
 const logSymbols = require('log-symbols');
+const chalk = require('chalk');
 const awsSwaggerImport = require('aws-swagger-import');
 
 const cli = meow(`
@@ -10,12 +11,13 @@ const cli = meow(`
 	  $ aws-swagger [action] [file]
 
 	Options
-	  --name     The name of the API Gateway
-	  --profile  The local AWS profile
-	  --region   The AWS region [Default: us-west-1]
+	  --name          The name of the API Gateway
+	  --aws-profile   The local AWS profile
+	  --aws-region    The AWS region [Default: us-west-1]
+		--aws-filename  The credentials filename [Default: ~/.aws/credentials]
 
 	Examples
-	  $ aws-swagger import swagger.json --name=gateway-name --profile=local-aws-profile
+	  $ aws-swagger import swagger.json --name=gateway-name --aws-profile=local-aws-profile
 `);
 
 if (cli.input.length !== 2) {
@@ -31,8 +33,8 @@ if (['import'].indexOf(cli.input[0]) === -1) {
 const filePath = path.resolve(process.cwd(), cli.input[1]);
 
 awsSwaggerImport(filePath, cli.flags)
-	.then(() => {
-		console.log(`  ${logSymbols.success}  done`);
+	.then(result => {
+		console.log(`  ${logSymbols.success}  ${chalk.cyan(result.name)} imported successfully ${chalk.gray('(' + result.id + ')')}`);
 	})
 	.catch(err => {
 		console.error(`  ${logSymbols.error}  ${err.message}`);
